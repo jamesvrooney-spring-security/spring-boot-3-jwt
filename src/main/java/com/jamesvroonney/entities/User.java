b2f765799dcc03650a4d3d8995b0ebc269117702
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-04 17:57:36 +0000

    Created User entity and SecurityUser (UserDetails implementation).

---
+++ src/main/java/com/jamesvroonney/entities/User.java
@@ -0,0 +1,25 @@
+package com.jamesvroonney.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
---
+++ src/main/resources/scripts.db/create-users-database.sql
@@ -0,0 +1,6 @@
+create table users (
+    id bigint not null primary key,
+    email varchar(255),
+    password varchar(255),
+    username varchar(255)
+)
