e32fd9a7d087672611631b8de02009a79045e725
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-04 15:11:38 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvroonney/SpringBoot3JwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvroonney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3JwtExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesvroonney/SpringBoot3JwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesvroonney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
